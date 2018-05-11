e442be6f934c34447cf39cca58745499fc50fd71
Rob Remington <robrem@uw.edu>
2018-05-11 14:06:59 -0700

    01: pythonize answers

--- 01/wordvalue.py
+++ 01/wordvalue.py
@@ -10,22 +10,12 @@ def load_words():
 def calc_word_value(word):
     """Calculate the value of the word entered into function
     using imported constant mapping LETTER_SCORES"""
-    score = 0
-    for char in word:
-        score += LETTER_SCORES.get(char.upper(), 0)
-    return score
+    return sum(LETTER_SCORES.get(char.upper(), 0) for char in word)
 
 def max_word_value(words=load_words()):
     """Calculate the word with the max value, can receive a list
     of words as arg, if none provided uses default DICTIONARY"""
-    max_value = 0
-    max_word = ''
-    for word in words:
-        word_value = calc_word_value(word)
-        if word_value > max_value:
-            max_value = word_value
-            max_word = word
-    return max_word
+    return max(words, key=calc_word_value)
 
 if __name__ == "__main__":
     pass # run unittests to validate
