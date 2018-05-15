abdbba2c252de0ec073b687d647bdba0162799df
Rob Remington <robrem@users.noreply.github.com>
2018-05-15 14:23:05 -0700

    refactor: get_tags() returns list of all tags, including dupes; get_top_tags() uses Counter; get_similarities uses itertools instead of nested for loops

--- 03/tags.py
+++ 03/tags.py
@@ -1,6 +1,8 @@
-import operator
+
 import xml.etree.ElementTree as ET
-from collections import defaultdict
+import itertools
+from difflib import SequenceMatcher
+from collections import Counter
 
 TOP_NUMBER = 10
 RSS_FEED = 'rss.xml'
@@ -10,33 +12,36 @@ SIMILAR = 0.87
 def get_tags():
     """Find all tags in RSS_FEED.
     Replace dash with whitespace."""
-    tags = defaultdict(int)
+    tags = []
     e = ET.parse(RSS_FEED).getroot()
     for category in e.iter('category'):
-        tags[category.text] += 1
+        tags.append(category.text.replace('-', ' '))
     return tags
 
 
 def get_top_tags(tags):
     """Get the TOP_NUMBER of most common tags"""
-    sorted_tags = sorted(tags.items(), key=operator.itemgetter(1), reverse=True)
-    return sorted_tags[:TOP_NUMBER]
+    tags = Counter(tags)
+    return tags.most_common(TOP_NUMBER)
 
 
 def get_similarities(tags):
     """Find set of tags pairs with similarity ratio of > SIMILAR"""
-    pass
-
+    similar_tags = []
+    tags = list(set(tags))
+    for pair in itertools.combinations(tags, 2):
+        if SequenceMatcher(None, *pair).ratio() > SIMILAR:
+            similar_tags.append(sorted(pair))
+    return similar_tags
 
 if __name__ == "__main__":
-    # tags = get_tags()
-    # top_tags = get_top_tags(tags)
-    # print('* Top {} tags:'.format(TOP_NUMBER))
-    # for tag, count in top_tags:
-    #     print('{:<20} {}'.format(tag, count))
-    # similar_tags = dict(get_similarities(tags))
-    # print()
-    # print('* Similar tags:')
-    # for singular, plural in similar_tags.items():
-    #     print('{:<20} {}'.format(singular, plural))
-    pass
+    tags = get_tags()
+    top_tags = get_top_tags(tags)
+    print('* Top {} tags:'.format(TOP_NUMBER))
+    for tag, count in top_tags:
+        print('{:<20} {}'.format(tag, count))
+    similar_tags = dict(get_similarities(tags))
+    print()
+    print('* Similar tags:')
+    for singular, plural in similar_tags.items():
+        print('{:<20} {}'.format(singular, plural))
