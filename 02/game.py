dc363cf6a4fde58197292ad955d7f8891dd9e5f8
Rob Remington <robrem@users.noreply.github.com>
2018-05-15 12:11:43 -0700

    convert to SimpleScrabble class

--- 02/game.py
+++ 02/game.py
@@ -9,60 +9,68 @@ import random
 
 NUM_LETTERS = 7
 
+class SimpleScrabble:
 
-# re-use from challenge 01
-def calc_word_value(word):
-    """Calc a given word value based on Scrabble LETTER_SCORES mapping"""
-    return sum(LETTER_SCORES.get(char.upper(), 0) for char in word)
+    def __init__(self):
+        self.draw = self.draw_letters()
 
-# re-use from challenge 01
-def max_word_value(words):
-    """Calc the max value of a collection of words"""
-    return max(words, key=calc_word_value)
 
-def draw_letters():
-    """Return NUM_LETTERS letters randomly selected from POUCH"""
-    return random.sample(POUCH, NUM_LETTERS)
+    # re-use from challenge 01
+    def calc_word_value(self, word):
+        """Calc a given word value based on Scrabble LETTER_SCORES mapping"""
+        return sum(LETTER_SCORES.get(char.upper(), 0) for char in word)
 
-def get_possible_dict_words(letters):
-    """Return list of words from DICTIONARY that can be made from letters"""
-    return [x for x in _get_permutations_draw(letters) if x.lower() in DICTIONARY]
+    # re-use from challenge 01
+    def max_word_value(self, words):
+        """Calc the max value of a collection of words"""
+        return max(words, key=self.calc_word_value)
 
-def _get_permutations_draw(letters):
-    """Return set of all permutations of letters of length 1 through NUM_LETTERS inclusive"""
-    perms =  [list(itertools.permutations(letters, n)) for n in range(1, NUM_LETTERS + 1)]
-    return set([''.join(x) for perm in perms for x in perm])
+    def draw_letters(self):
+        """Return NUM_LETTERS letters randomly selected from POUCH"""
+        return random.sample(POUCH, NUM_LETTERS)
 
-def _validation(guess, draw):
-    """Ensure that all letters of guess are in draw, and guess is in DICTIONARY"""
-    if len(guess) > len(draw):
-        return False
-    if guess.lower() not in DICTIONARY:
-        return False
-    # Resulting Counter will be empty if guess CAN be made from draw
-    return not Counter(guess) - Counter(draw)
+    def get_possible_dict_words(self, letters):
+        """Return list of words from DICTIONARY that can be made from letters"""
+        return [x for x in self._get_permutations_draw(letters) if x.lower() in DICTIONARY]
 
-def calc_score(player_score, optimal_score):
-    return player_score / optimal_score
+    def _get_permutations_draw(self, letters):
+        """Return set of all permutations of letters of length 1 through NUM_LETTERS inclusive"""
+        perms =  [list(itertools.permutations(letters, n)) for n in range(1, NUM_LETTERS + 1)]
+        return set([''.join(x) for perm in perms for x in perm])
+
+    def _validation(self, guess, draw):
+        """Ensure that all letters of guess are in draw, and guess is in DICTIONARY"""
+        if len(guess) > len(draw):
+            return False
+        if guess.lower() not in DICTIONARY:
+            return False
+        # Resulting Counter will be empty if guess CAN be made from draw
+        return not Counter(guess) - Counter(draw)
+
+    def calc_score(self, player_score, optimal_score):
+        return round(player_score / optimal_score, 1)
+
+    def play(self):
+        print(f'Letters drawn: {", ".join(self.draw)}')
+
+        while True:
+            guess = input('Form a valid word: ').upper()
+            if not self._validation(guess, self.draw):
+                print('Invalid guess. Try again.')
+            else:
+                break
+
+        guess_value = self.calc_word_value(guess)
+        print(f'Word chosen: {guess} (value: {guess_value})')
+        optimal_word = self.max_word_value(self.get_possible_dict_words(self.draw))
+        optimal_value = self.calc_word_value(optimal_word)
+        print(f'Optimal word possible: {optimal_word} (value: {optimal_value})')
+        score = self.calc_score(guess_value, optimal_value)
+        print(f'You scored: {score}')
 
 def main():
-    draw = draw_letters()
-    print(f'Letters drawn: {", ".join(draw)}')
-
-    while True:
-        guess = input('Form a valid word: ').upper()
-        if not _validation(guess, draw):
-            print('Invalid guess. Try again.')
-        else:
-            break
-
-    guess_value = calc_word_value(guess)
-    print(f'Word chosen: {guess} (value: {guess_value})')
-    optimal_word = max_word_value(get_possible_dict_words(draw))
-    optimal_value = calc_word_value(optimal_word)
-    print(f'Optimal word possible: {optimal_word} (value: {optimal_value})')
-    score = round(calc_score(guess_value, optimal_value), 1)
-    print(f'You scored: {score}')
+    game = SimpleScrabble()
+    game.play()
 
 if __name__ == "__main__":
     main()
